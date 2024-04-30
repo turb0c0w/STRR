3afa17a72ad07dbca1ff3ab448f18ca1b90677f1
Sam Schantz <samschantz@Sams-MacBook-Pro.local>
2024-04-30 12:56:38 -0700

    fixing linting

--- strr-web/components/bcros/letter-icon/LetterIcon.vue
+++ strr-web/components/bcros/letter-icon/LetterIcon.vue
@@ -4,7 +4,7 @@
     color="primary"
     class="pointer-events-none bg-blue-350 h-[32px] w-[32px] text-[19px] flex font-bold justify-center"
     :inactive="true"
-    tabindex='-1'
+    tabindex="-1"
     role="none"
   />
 </template>
