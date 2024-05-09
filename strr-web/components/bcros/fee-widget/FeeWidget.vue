6cbbe68ac5491f51b5f304ce3e1845bee47d40fe
Sam Schantz <samschantz@Sams-MacBook-Pro.local>
2024-05-09 07:30:32 -0700

    adding mobile fee widget

--- strr-web/components/bcros/fee-widget/FeeWidget.vue
+++ strr-web/components/bcros/fee-widget/FeeWidget.vue
@@ -3,10 +3,13 @@
     data-cy="fee-widget"
     class="w-[280px] rounded-[4px] font-bold text-black bg-white"
   >
-    <div class="px-[15px] py-[10px] pt-[10px] text-white bg-blue-550 rounded-t-[4px]">
+    <div class="px-[15px] py-[10px] pt-[10px] text-white bg-blue-550 rounded-t-[4px] flex flex-row justify-between">
       <p>{{ t("fee-widget.summary") }}</p>
+      <div class="mobile:hidden" @click="toggleFolded">
+        <img src="/icons/caret.svg" :class="`cursor-pointer transition-all ${folded ? 'rotate-180': ''}`">
+      </div>
     </div>
-    <div class="px-[15px] pb-[10px]">
+    <div :class="`transition-all ${folded ? 'h-[0px] overflow-hidden p-[0px]': 'px-[15px] pb-[10px] '}`">
       <p class="py-[10px] border-b-[1px] border-bcGovGray-300">
         {{ t("fee-widget.registration-fee") }}
       </p>
@@ -23,5 +26,10 @@
 <script setup lang="ts">
 
 const t = useNuxtApp().$i18n.t
+const folded = ref(false)
+
+const toggleFolded = () => {
+  folded.value = !folded.value
+}
 
 </script>
