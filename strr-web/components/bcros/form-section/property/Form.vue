80f22412fb19ccc87b16d89cc81d4d1f8d7c3caa
Sam Schantz <samschantz@Sams-MacBook-Pro.local>
2024-05-29 09:56:36 -0700

    adding fix for nickname

--- strr-web/components/bcros/form-section/property/Form.vue
+++ strr-web/components/bcros/form-section/property/Form.vue
@@ -9,6 +9,7 @@
       <UForm :schema="propertyDetailsSchema" :state="formState.propertyDetails">
         <BcrosFormSectionPropertyAddress
           id="propertyAddress"
+          v-model:nickname="formState.propertyDetails.nickname"
           v-model:country="formState.propertyDetails.country"
           v-model:address="formState.propertyDetails.address"
           v-model:address-line-two="formState.propertyDetails.addressLineTwo"
