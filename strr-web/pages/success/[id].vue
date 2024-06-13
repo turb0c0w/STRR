af241e17212b40051c02b4c5878dd089c44b7cb9
Sam Schantz <samschantz@Sams-MacBook-Pro.local>
2024-06-13 09:05:39 -0700

    adding style fixes to application-confirm

--- strr-web/pages/success/[id].vue
+++ strr-web/pages/success/[id].vue
@@ -1,6 +1,6 @@
 <template>
   <div data-cy="account-select-page">
-    <BcrosTypographyH1 text="Application Submitted" data-cy="accountPageTitle" class="mobile:pb-[20px]" />
+    <BcrosTypographyH1 text="Application Submitted" data-cy="accountPageTitle" class="mobile:pb-[20px] pb-[32px]" />
     <div class="bg-white py-[22px] px-[30px] flex flex-row">
       <img
         class="self-start mr-[10px] mt-[2px]"
@@ -17,6 +17,17 @@
         <p>Your application will be reviewed by our team and we will email you with next steps.</p>
       </div>
     </div>
+    <BcrosTypographyH2 text="Helpful Links" class="mt-[32px] text-[18px] mb-[24px]" />
+    <p class="mb-[24px]">
+      <a>View your application status</a> in your dashboard
+    </p>
+    <p class="mb-[24px]">
+      <a>Visit your BC Registries Online Services profile page</a> to update your profile information
+    </p>
+    <p class="mb-[24px]">
+      Have another rental property?
+    </p>
+    <BcrosButtonsPrimary text="Start a new application" :action="redirectToCreate" class-name="font-bold" />
   </div>
 </template>
 
@@ -30,6 +41,10 @@ const id = route.params.id
 
 const apiURL = useRuntimeConfig().public.strrApiURL
 const axiosInstance = addAxiosInterceptors(axios.create())
+
+const redirectToCreate = () => {
+  navigateTo('/create-account')
+}
 
 axiosInstance.get(`${apiURL}/registrations`)
   .then((res) => {
