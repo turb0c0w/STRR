fd1bc3ef5710a3a232ba136f262fd244042ff00f
Sam Schantz <samschantz@Sams-MacBook-Pro.local>
2024-06-13 09:16:13 -0700

    fixing linting issues

--- strr-web/pages/success/[id].vue
+++ strr-web/pages/success/[id].vue
@@ -1,33 +1,41 @@
 <template>
   <div data-cy="account-select-page">
-    <BcrosTypographyH1 text="Application Submitted" data-cy="accountPageTitle" class="mobile:pb-[20px] pb-[32px]" />
-    <div class="bg-white py-[22px] px-[30px] flex flex-row">
+    <BcrosTypographyH1
+      text="Application Submitted"
+      data-cy="accountPageTitle"
+      class="mobile:pb-[20px] mobile:mx-[8px] pb-[32px]"
+    />
+    <div class="bg-white py-[22px] px-[30px] flex flex-row mobile:px-[8px] mobile:[py-16px] mobile:flex-col">
       <img
-        class="self-start mr-[10px] mt-[2px]"
+        class="self-start mr-[10px] mt-[2px] mobile:[mb-8px]"
         src="/icons/create-account/check_circle.svg"
         alt="Confirmation check mark"
       >
       <div>
-        <p class="mb-[24px]">
+        <p class="mb-[24px] mobile:[mb-8px]">
           Short term registry application submitted for the following property:
         </p>
         <p class="mb-[24px] font-bold">
           {{ fetchedRegistration ? fetchedRegistration.unitAddress.address : '-' }}
         </p>
-        <p>Your application will be reviewed by our team and we will email you with next steps.</p>
+        <p class="mobile:mb-[24px]">
+          Your application will be reviewed by our team and we will email you with next steps.
+        </p>
       </div>
     </div>
-    <BcrosTypographyH2 text="Helpful Links" class="mt-[32px] text-[18px] mb-[24px]" />
-    <p class="mb-[24px]">
-      <a>View your application status</a> in your dashboard
-    </p>
-    <p class="mb-[24px]">
-      <a>Visit your BC Registries Online Services profile page</a> to update your profile information
-    </p>
-    <p class="mb-[24px]">
-      Have another rental property?
-    </p>
-    <BcrosButtonsPrimary text="Start a new application" :action="redirectToCreate" class-name="font-bold" />
+    <div class="mobile:mx-[8px]">
+      <BcrosTypographyH2 text="Helpful Links" class="mt-[32px] text-[18px] mb-[24px]" />
+      <p class="mb-[24px]">
+        <a @click="navigateTo('/application-status')">View your application status</a> in your dashboard
+      </p>
+      <p class="mb-[24px]">
+        <a @click="navigateTo('/profile-page')">Visit your BC Registries Online Services profile page</a> to update your profile information
+      </p>
+      <p class="mb-[24px]">
+        Have another rental property?
+      </p>
+      <BcrosButtonsPrimary text="Start a new application" :action="redirectToCreate" class-name="font-bold" />
+    </div>
   </div>
 </template>
 
