fdc60eb14a93f1997e5a64574bd05c4ee492cdfd
Sam Schantz <samschantz@Sams-MacBook-Pro.local>
2024-06-14 11:09:06 -0700

    adding localization and mobile fixes

--- strr-web/pages/application-status.vue
+++ strr-web/pages/application-status.vue
@@ -1,70 +1,76 @@
 <template>
   <div data-cy="account-select-page">
     <BcrosTypographyH1
-      text="My STR Registry Dashboard"
+      :text="tRegistrationStatus('title')"
       data-cy="accountPageTitle"
-      class="mobile:pb-[20px] mobile:mx-[8px] pb-[32px]"
+      class="mobile:pb-[20px] mobile:mx-[8px] pb-[32px] mobile:pb-[24px]"
     />
     <div>
-      <div class="flex flex-row justify-between">
-        <BcrosTypographyH2 text="My Registration Application" />
-        <BcrosButtonsPrimary text="Create New Registration" :action="() => navigateTo('/create-account')" icon="i-mdi-plus" />
+      <div class="px-[8px] flex flex-row justify-between">
+        <BcrosTypographyH2 :text="tRegistrationStatus('my-app')" class-name="mobile:pt-[0px]" />
+        <BcrosButtonsPrimary
+          :text="tRegistrationStatus('create')"
+          :action="() => navigateTo('/create-account')"
+          icon="i-mdi-plus"
+          class-name="mobile:hidden"
+        />
       </div>
-      <div class="flex flex-row flex-wrap justify-between">
+      <div class="flex flex-row mobile:flex-col flex-wrap justify-between">
         <div
           v-for="registration in registrations"
           :key="registration.id"
-          class="w-[calc(33%-20px)] mb-[42px] justify-between flex-col bg-white px-[30px] py-[22px] border-[2px] border-bcGovColor-hairlinesOnWhite"
+          class="
+            desktop:w-[calc(33%-20px)] mb-[42px] mobile:mb-[24px] justify-between flex-col
+            bg-white px-[30px] mobile:px-[8px] py-[22px]
+            border-[2px] border-bcGovColor-hairlinesOnWhite
+          "
         >
-          <div
-            :class="
-              `
-                bg-${getColor(registration.status).background}
-                text-${getColor(registration.status).text}
-                mb-[24px] font-bold px-[12px] py-[4px] flex-shrink flex w-fit rounded
-              `
-            "
-          >
-            {{ registration.status }}
-          </div>
-          <div class="mb-[24px]">
-            <p class="font-bold">
-              {{
-                registration.unitAddress.nickname
-                  ? registration.unitAddress.nickname
-                  : registration.unitAddress.address
-              }}
-            </p>
-            <p>
-              {{
-                registration.unitAddress.nickname
-                  ? registration.unitAddress.address
-                  : registration.unitAddress.addressLineTwo
-              }}
-            </p>
-            <p>
-              {{ registration.unitAddress.nickname ? registration.unitAddress.addressLineTwo : "" }}
-            </p>
-            <p>
-              {{
-                `
-                ${registration.unitAddress.city}
-                 ${registration.unitAddress.province}
-                 ${registration.unitAddress.postalCode},
-                 ${registration.unitAddress.country}
-                `
-              }}
-            </p>
+          <BcrosChip :flavour="getFlavour(registration.status)" class="mobile:hidden">
+            {{ tRegistrationStatus(registration.status) }}
+          </BcrosChip>
+          <div class="flex w-full justify-between">
+            <div class="mb-[24px]">
+              <p class="font-bold">
+                {{
+                  registration.unitAddress.nickname
+                    ? registration.unitAddress.nickname
+                    : registration.unitAddress.address
+                }}
+              </p>
+              <p>
+                {{
+                  registration.unitAddress.nickname
+                    ? registration.unitAddress.address
+                    : registration.unitAddress.addressLineTwo
+                }}
+              </p>
+              <p>
+                {{ registration.unitAddress.nickname ? registration.unitAddress.addressLineTwo : "" }}
+              </p>
+              <p>
+                {{
+                  `
+                  ${registration.unitAddress.city}
+                  ${registration.unitAddress.province}
+                  ${registration.unitAddress.postalCode},
+                  ${registration.unitAddress.country}
+                  `
+                }}
+              </p>
+            </div>
+            <BcrosChip :flavour="getFlavour(registration.status)" class="desktop: hidden">
+              {{ tRegistrationStatus(registration.status) }}
+            </BcrosChip>
           </div>
           <div class="flex flex-row text-bcGovColor-activeBlue justify-start">
             <p class="mr-[22px] cursor-pointer">
-              View
+              {{ tRegistrationStatus('view') }}
             </p>
             <p class="mr-[22px] cursor-pointer">
-              Download Certificate
+              {{ tRegistrationStatus('download') }}
             </p>
             <p class="cursor-pointer">
-              Renewal
+              {{ tRegistrationStatus('renewal') }}
             </p>
           </div>
         </div>
@@ -75,10 +81,12 @@
 
 <script setup lang="ts">
 import axios from 'axios'
-
 definePageMeta({
   layout: 'wide'
 })
+
+const t = useNuxtApp().$i18n.t
+const tRegistrationStatus = (translationKey: string) => t(`registration-status.${translationKey}`)
 
 const apiURL = useRuntimeConfig().public.strrApiURL
 const axiosInstance = addAxiosInterceptors(axios.create())
@@ -92,16 +100,16 @@ axiosInstance.get(`${apiURL}/registrations`)
     }
   })
 
-const getColor = (status: string) => {
+const getFlavour = (status: string) => {
   switch (status) {
     case 'DENIED':
-      return { text: 'red-500', background: 'red-100' }
+      return AlertsFlavourE.ALERT
     case 'APPROVED':
-      return { text: 'green-500', background: 'green-100' }
+      return AlertsFlavourE.SUCCESS
     case 'PENDING':
-      return { text: 'orange-500', background: 'orange-100' }
+      return AlertsFlavourE.WARNING
     default:
-      return { text: 'orange-500', background: 'yellow-50' }
+      return AlertsFlavourE.INFO
   }
 }
 
