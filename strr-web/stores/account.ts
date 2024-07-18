7dc7a5c4d6a392db5f89605561b0cacb9f8fe7ce
Sam Schantz <samschantz@sams-mbp.home>
2024-07-18 08:44:23 -0300

    adding redirect without doc upload

--- strr-web/stores/account.ts
+++ strr-web/stores/account.ts
@@ -81,7 +81,7 @@ export const useBcrosAccount = defineStore('bcros/account', () => {
       .then(() => {
         setAccountInfo()
           .then(() => {
-            navigateTo('/account-select')
+            navigateTo('/create-account')
           })
       })
   }
