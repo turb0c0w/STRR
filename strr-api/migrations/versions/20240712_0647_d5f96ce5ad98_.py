da5641ae56ac219297b7072e3494373ef5ee9403
Kris <kris@daxiom.com>
2024-07-20 00:39:00 -0700

    Rebase

--- strr-api/migrations/versions/20240712_0647_d5f96ce5ad98_.py
+++ strr-api/migrations/versions/20240712_0647_d5f96ce5ad98_.py
@@ -11,7 +11,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = 'd5f96ce5ad98'
-down_revision = 'ed33e07431ea'
+down_revision = '269514c7d861'
 branch_labels = None
 depends_on = None
 
